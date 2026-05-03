02a45141c0fb3b5a1b4ff08e85293b5727402245
Hariz S. <me@bomberfish.ca>
2026-05-03 02:45:01 -0400

    refreshed checkbox

--- packages/chrome/src/components/Checkbox.tsx
+++ packages/chrome/src/components/Checkbox.tsx
@@ -43,19 +43,28 @@ Checkbox.style = css`
 		align-items: center;
 		justify-content: center;
 		color: var(--toolbar_field);
+		text-shadow: 0 0.055em 0.05em var(--accent-shade-20);
 		font-size: 0.8em;
 		transform: scale(0);
-		transition: transform 120ms ease;
+		transition: transform 120ms cubic-bezier(0.43, 0.91, 0.34, 1.3);
 		pointer-events: none;
 	}
 
 	:scope:has(input:checked) {
-		background: var(--tab_line);
-		border-color: var(--tab_line);
+		background-color: var(--tab_line);
+		background-image: radial-gradient(
+			70% 50% at 50% 100%,
+			var(--accent-tint-20),
+			var(--accent-shade-10)
+		);
+		box-shadow:
+			inset 0px 0.05em 0.025em rgb(255 255 255 / 40%),
+			inset 0px -0.05em 0.04em rgb(0 0 0 / 20%);
+		border: none;
 	}
 
 	:scope:has(input:checked)::after {
-		transform: scale(1);
+		transform: scale(1) translateY(0.5px);
 	}
 
 	input {
